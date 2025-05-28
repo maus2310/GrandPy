c56ac1318dfbe90d20a375ec5f7910944d4fe20f
wea55367 <andreas.weiss@stud.uni-regensburg.de>
2025-05-28 16:54:52 +0200

    Modified plot_scatter function

--- Py/plot.py
+++ Py/plot.py
@@ -6,19 +6,22 @@ from typing import Optional
 from Py.load import *
 from scipy.stats import gaussian_kde
 
-def plot_scatter(data,
+def plot_scatter(data: GrandPy,
                         x: str,
                         y: str,
                         mode_slot: str | ModeSlot = None,
-                        remove_outlier: bool = False,
-                        show_outlier: bool = True,
+                        remove_outlier: bool = True,
+                        show_outlier: bool = True,              #Funktioniert noch nicht richtig
+                        path_for_save: str = None,
                         xlim: Optional[tuple[float, float]] = None,
-                        ylim: Optional[tuple[float, float]] = None,):
+                        ylim: Optional[tuple[float, float]] = None):
     """
         ScatterPlot
 
         Parameters
         ----------
+        data: GrandPy
+            Object of GrandPy class
         x: str
             an expression to compute the x value or a character corresponding to a sample (or cell) name or a fully qualified analysis result name
         y: str
@@ -27,16 +30,27 @@ def plot_scatter(data,
             Count, Ntr ...
         remove_outlier: bool
             Detects and removes outliers
+        path_for_save: str
+            saves the plot to the specified path
         xlim: tuple[float, float]
-            define the x axis limits (defining the lower and upper bound)
+            define the x-axis limits (defining the lower and upper bound)
         ylim: tuple[float, float]
-            define the y axis limits (defining the lower and upper bound)
+            define the y-axis limits (defining the lower and upper bound)
 
         Returns
         -------
         Plot:
             Plot object containing the scatter plot
         """
+
+    # error messages
+    if x not in data.coldata["Name"].tolist():
+        raise ValueError(f"x is not a valid expression.")
+    if y not in data.coldata["Name"].tolist():
+        raise ValueError(f"y is not a valid expression.")
+
+
+
     matrix = data._resolve_mode_slot(mode_slot)
     if hasattr(matrix, "toarray"):
         matrix = matrix.toarray()
@@ -80,12 +94,12 @@ def plot_scatter(data,
     idx = kde.argsort()
     x_vals, y_vals, kde = x_vals[idx], y_vals[idx], kde[idx]
 
-    plt.figure(figsize=(6, 6))
+    plt.figure(figsize=(10, 10))
     if remove_outlier and show_outlier:
         x_out = x_vals_all[~mask_keep]
         y_out = y_vals_all[~mask_keep]
-        plt.scatter(x_out, y_out, color="lightgray", s=1, alpha=0.5, label="Outliers")
-    scatter = plt.scatter(x_vals, y_vals, c=kde, s=5, cmap='viridis', alpha=0.8)
+        plt.scatter(x_out, y_out, color="lightgray", s=5, alpha=0.5, label="Outliers")
+    scatter = plt.scatter(x_vals, y_vals, c=kde, s=10, cmap='viridis', alpha=0.8)
     plt.xlabel(x)
     plt.ylabel(y)
     plt.title(f"{x} vs {y} ({mode_slot})")
@@ -97,5 +111,8 @@ def plot_scatter(data,
         plt.ylim(ylim)
 
     plt.grid(False)
-    #plt.tight_layout()
+    #offsets = scatter.get_offsets()
+    #print(len(offsets))
+    if path_for_save is not None :
+        plt.savefig(f"{path_for_save}{x}_{y}_{mode_slot}.png", format="png", dpi=300)
     plt.show()
