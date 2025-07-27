36fbfd883a6b0d169d59c5d3d91a08ad7b983378
Kathi Steinkirchner <katharina.steinkirchner@stud.uni-regensburg.de>
2025-07-27 17:51:01 +0200

    updated the logo, cheatsheet & init

--- grandpy/__init__.py
+++ grandpy/__init__.py
@@ -1,6 +1,6 @@
 from .grandPy import GrandPy
 from .grandPy import anndata_to_grandpy, read_h5ad
-from .load import read_grand, classify_genes, semantics_time, parse_time_string
+from .load import DESIGN_KEYS, semantics_time, build_coldata, read_grand, classify_genes, parse_time_string
 from .plot import (
     plot_scatter,
     plot_heatmap,
@@ -29,6 +29,8 @@ __all__ = [
     "classify_genes",
     "semantics_time",
     "parse_time_string",
+    "DESIGN_KEYS",
+    "build_coldata",
     "read_grand",
     "plot_scatter",
     "plot_heatmap",
